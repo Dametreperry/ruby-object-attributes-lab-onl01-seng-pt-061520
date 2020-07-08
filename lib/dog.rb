50ffa6f4d41769b7de103243b1f2c156b184d586
DAMETRES PERRY <dametres.perry21@gmail.com>
2020-07-08 03:11:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,17 +1,18 @@
 class Dog 
-  def name=(dog_name)
-    @the_dogs_name = dog_name
-  end
-  
-  def name
-    @the_dogs_name
-  end
-  
-  def breed=(dog_breed)
-    @the_dogs_breed = dog_breed
-  end
-  
-  def breed
-    @the_dogs_breed
-  end
-end
+
+def name=(name)
+  @name = name
+end 
+
+def name
+  @name
+end 
+
+def breed=(breed)
+  @breed = breed
+end 
+
+def breed
+  @breed
+end 
+end 
